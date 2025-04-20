ba01e3d78bb8f4f777f46dfc51f04176e74cc071
alter-a <your@email.com>
2025-04-20 10:22:38 +0800

    Revert "fuami"
    This reverts commit 280a1d7e50421730c23a1a8f522cba272788a810.

--- database/seeders/ProfilesTableSeeder.php
+++ database/seeders/ProfilesTableSeeder.php
@@ -4,48 +4,27 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
+use App\Models\Profile;
 use App\Models\User;
 
-class UsersTableSeeder extends Seeder
+class ProfilesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        User::create([
-            'username' => 'zear',
-            'email' => 'developer@zear.com',
-            'password' => Hash::make('password'),
-            'role_id' => 1, 
-            'isDeleted' => false,
-            'isActive' => true,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-
-
-        User::create([
-            'username' => 'admin',
-            'email' => 'admin@zear.com',
-            'password' => Hash::make('password'),
-            'role_id' => 2, 
-            'isDeleted' => false,
-            'isActive' => true,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-
-
-
-          User::create([
-            'username' => 'cashier',
-            'email' => 'cashier@zear.com',
-            'password' => Hash::make('password'),
-            'role_id' => 3, 
-            'isDeleted' => false,
-            'isActive' => true,
+            Profile::create([
+            'user_id' => User::where('username', 'admin')->first()->id,
+            'firstname' => 'Zhie',
+            'lastname' => 'Bautista',
+            'phone_number' => '9277294457',
+            'address' => 'Doongan',
+            'profile_picture' => 'path/to/pic.jpg',
+            'birthdate' => '1993-10-13',
+            'gender' => 'Male',
+            'nationality' => 'Filipino',
+            'bio' => 'Developer.',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
