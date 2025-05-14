6f744b391b109ccd08f75be568309a9a11a3da7e
royalguard14 <ghaizar.bautista@aclcbutuan.edu.ph>
2025-05-15 07:58:05 +0800

    hello

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -340,9 +340,7 @@ public function records()
     $cashierName =[];
         return response()->json([
         'status' => 'success',
-        'payments' => $payments,
-        'month_years' => $monthYears,
-        'cashier_name' => $cashierName
+
     ]);
 
 
